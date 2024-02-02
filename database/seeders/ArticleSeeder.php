bfeba8559b5300600f22899a6b4d9953bb038cf6
israr <misrar09@gmail.com>
2024-02-02 08:46:24 +0100

    article seeder included

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,30 @@ class ArticleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $articles = [
+            [
+
+                'title' => 'First Article',
+                'description' => 'This is the first articles description',
+                'conclustion' => 'This is the conclustion of the first article'
+            ],
+            [
+                'title' => 'Second Article',
+                'description' => 'This is the second articles description',
+                'conclustion' => 'This is the conclustion of the second article'
+            ],
+            [
+                'title' => 'Third Article',
+                'description' => 'This is the third articles description',
+                'conclustion' => 'This is the conclustion of the third article'
+            ],
+
+        ];
+
+        foreach ($articles as $article) {
+            $newArticle = new Article();
+            $newArticle->fill($article);
+            $newArticle->save();
+        }
     }
 }
